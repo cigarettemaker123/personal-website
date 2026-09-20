9bd9fcfae0f30d79dd180075904dc092ac9b8d0b
natalie <nataliepuha29@gmail.com>
2026-09-20 04:04:31 -0700

    first commit yay

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <div className="flex w-full max-w-5xl items-center justify-center gap-4">
+          <div>hey im natalie (totally)</div>
+        </div>
+      </main>
+    </div>
+  );
+}
